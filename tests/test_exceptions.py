89742aaeb6931bcd12d3b6585ff2b36a278030b5
Frank Atencio Loreth <48834633+FrankAtencioLoreth@users.noreply.github.com>
2025-02-26 16:45:29 -0500

    Add exeptions

---
+++ tests/test_exceptions.py
@@ -0,0 +1,15 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service as EdgeService
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
+driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
+
+def test_validate_equal_text(driver):
+    try:
+        element = driver.find_element_by_id("element_id")
+        expect_text = "example"
+
+        assert element.text == expect_text
+    except NoSuchElementExeption:
+        assert False, "Could not find the element with the supplied locator"
